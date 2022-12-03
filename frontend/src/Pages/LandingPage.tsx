4b0310f9febf31ae845d64ad98e7f165e83f59c8
Nathan Liu <nathanliu17@gmail.com>
2022-12-03 14:43:15 -0800

    rout

---
+++ frontend/src/Pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+function LandingPage() {
+        return (
+		<p>
+			Hello World.
+		</p>
+        );
+}
+
+export default LandingPage;
