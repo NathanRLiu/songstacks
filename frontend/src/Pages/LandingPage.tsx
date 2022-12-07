c9cb1a35812bd93076d7a422385cbd518f71461c
Nathan Liu <nathanliu17@gmail.com>
2022-12-06 23:47:35 -0800

    revamped landing page

--- frontend/src/Pages/LandingPage.tsx
+++ frontend/src/Pages/LandingPage.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, useNavigate } from 'react-router-dom';
 import { GiMicrophone } from 'react-icons/gi';
 import { HiUserGroup } from 'react-icons/hi2';
 import { ImHeadphones } from 'react-icons/im';
+import {Button} from 'react-bootstrap';
 import logo from '../songstacks.png';
 
 import styles from '../Styles/LandingPage.module.css'
@@ -13,42 +14,49 @@ function LandingPage() {
 		<div className={ styles.background }>
 			<div className={ styles.diffuser }>
 				<div className={ styles.glow1 + " " + styles.glow } />
-				<div className={ styles.glow2 + " " + styles.glow } />
-				<div className={ styles.glow3 + " " + styles.glow } />
-				<div className={ styles.glow4 + " " + styles.glow } />
-				<div className={ styles.glow5 + " " + styles.glow } />
 			</div>
-			<div className={ styles["title-text"] }>
-				<h1>Introducing SongStacks.</h1>
-			</div>
-			<p className={styles["description-text"]}> Let's redefine the music industry.</p>
-			<div className={ styles.section2 }>
-				<div className={ styles.card }>
-					<i className={ styles.icon }><GiMicrophone /></i>
-					<p className={ styles.description }>
-						SongStacks lets new artists upload their work and start making money from it right away
-					</p>
+			<div className={ styles["above-diffuser"] }>
+				<div className={ styles["header"] } >
+					<div className={styles.brand}>
+						<img src={logo} />
+						<h1> SongStacks</h1>
+					</div>
+					{/*<div className={styles.contact}>
+						<Button variant="outline-primary"> Contact Us </Button>
+					</div>*/}
 				</div>
-				<div className={ styles.card }>
-					<i className={ styles.icon }> <HiUserGroup /> </i>
-					<p className={ styles.description }>
-						SongStacks makes it easy to find other people to collaborate with!
-					</p>
+				<div className={ styles["title-text"] }>
+					<h1>Introducing SongStacks.</h1>
 				</div>
-				<div className={ styles.card }>
-					<i className={ styles.icon }> <ImHeadphones /> </i>	
-					<p className={ styles.description }>
-						SongStacks lets you easily discover new music from the genres you love
-					</p>
+				<p className={styles["description-text"]}> Let's redefine the music industry.</p>
+				<div className={ styles.section2 }>
+					<div className={ styles.card }>
+						<i className={ styles.icon }><GiMicrophone /></i>
+						<p className={ styles.description }>
+							Create new music and start making money off it right away
+						</p>
+					</div>
+					<div className={ styles.card }>
+						<i className={ styles.icon }> <HiUserGroup /> </i>
+						<p className={ styles.description }>
+							Connect with a community of users ready to make new sounds
+						</p>
+					</div>
+					<div className={ styles.card }>
+						<i className={ styles.icon }> <ImHeadphones /> </i>	
+						<p className={ styles.description }>
+							Discover new music from the genres you love
+						</p>
+					</div>
 				</div>
-			</div>
-			<div className={ styles.section3 }>
-				<h2> Ready to get started? </h2>
-				<div className={ styles.buttons }>
-					<button onClick={
-						() => {navigate("/login")}
-					}> Start Creating </button>
-					<button> Start Listening </button>
+				<div className={ styles.section3 }>
+					<h2> Ready to get started? </h2>
+					<div className={ styles.buttons }>
+						<button onClick={
+							() => {navigate("/login")}
+						}> Start Creating </button>
+						<button> Start Listening </button>
+					</div>
 				</div>
 			</div>
 		</div>
