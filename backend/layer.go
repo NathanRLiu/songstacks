7b0d0548d8233ffebac7651020a9155fe2e6f465
Chinmay Devadhar <cdevadhar@gmail.com>
2022-12-06 17:23:16 -0800

    fixed id thing

--- backend/layer.go
+++ backend/layer.go
@@ -12,12 +12,11 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/bson"
-	guuid "github.com/google/uuid"
 	// "go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type Layer struct {
-	_id		string
+	ID		primitive.ObjectID `bson:"_id"`
 	ParentLayer	string		`form:"parent"`
 	ChildLayers	[]string	`form:"children"`
 	LayerAudio	[]byte	`form:"file"`
@@ -30,7 +29,7 @@ type getLayerReq struct {
 func getLayer(c *gin.Context) {
 	//var res [][]byte
 	layerID, _ := primitive.ObjectIDFromHex(c.Query("layerid"))
-
+	log.Printf(c.Query("layerid"))
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	coll := client.Database("songDB").Collection("layers")
 
@@ -38,11 +37,12 @@ func getLayer(c *gin.Context) {
 	var layers []Layer
 	err = coll.FindOne(context.TODO(), bson.M{"_id": layerID}).Decode(&curr)
 	if err != nil {
+		log.Printf(err.Error());
 		c.JSON(http.StatusNotFound, gin.H{"Success": false})
 		return
 	}
 
-		layers = append(layers, curr)
+	layers = append(layers, curr)
 	for curr.ParentLayer != "" {
 		
 		_ = coll.FindOne(context.TODO(), bson.M{"_id": layerID}).Decode(&curr)
@@ -56,7 +56,6 @@ func getLayer(c *gin.Context) {
 }
 func createLayer(c *gin.Context) {
 	file_not_binary,_, _ := c.Request.FormFile("file")
-
 	buf := bytes.NewBuffer(nil)
 	if _, err := io.Copy(buf, file_not_binary); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"Success": false})
@@ -78,8 +77,8 @@ func createLayer(c *gin.Context) {
 			panic(merr)
 		}
 	}()
-	id := guuid.New().String()
-    id = strings.Replace(id, "-", "", -1)
+	objectID := primitive.NewObjectID()
+	id := objectID.Hex()
 	coll := client.Database("songDB").Collection("layers")
 	if (parent!="") {
 		var parentLayer Layer
@@ -92,15 +91,17 @@ func createLayer(c *gin.Context) {
 		}
 		parentChildren := parentLayer.ChildLayers
 		parentChildren = append(parentChildren, id)
+		log.Printf(id);
+		log.Printf(strings.Join(parentChildren, " "))
 		coll.UpdateOne(
 			context.TODO(),
 			bson.D{{"_id", parentID}},
-			bson.D{{"$set", bson.D{{"childLayers", parentChildren}}}},
+			bson.D{{"$set", bson.D{{"childlayers", parentChildren}}}},
 		)
 
 	}
 	
-	newLayer := Layer{_id:id, ParentLayer: parent, LayerAudio: file, LayerCut: 0, ChildLayers:make([]string, 0)}
+	newLayer := Layer{ID:objectID, ParentLayer: parent, LayerAudio: file, LayerCut: 0, ChildLayers:make([]string, 0)}
 	coll.InsertOne(context.TODO(), newLayer)
 
 	c.JSON(http.StatusOK, gin.H{"Success": true})
