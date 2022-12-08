52418f15426a85912a765cf8c3ad9b18eae333d9
Nathan Liu <nathanliu17@gmail.com>
2022-12-07 17:43:19 -0800

    real commit

--- frontend/src/Components/AudioPlayer.tsx
+++ frontend/src/Components/AudioPlayer.tsx
@@ -3,17 +3,16 @@ import { Buffer } from 'buffer';
 import ReactAudioPlayer from 'react-audio-player';
 import axios from 'axios';
 function AudioPlayer() {
-	const [audio, setAudio] = useState();
+	const [audio, setAudio] = useState(new Audio());
 	React.useEffect(() => {
 		const getUser = async () => {
 			setAudio(new Audio(`/api/layer/playSong/?songid=63903c52655d9e5074bdc619`));
-
 		}
 		getUser();
 	}, []);
 	return (
 		<>
-			{<div onClick={()=>{audio.play()}}style={{width:400,height:400}} />
+			{<div onClick={()=>{audio.play()}}style={{width:400,height:400, backgroundColor:"black"}} />
 
 			}
 		</>
