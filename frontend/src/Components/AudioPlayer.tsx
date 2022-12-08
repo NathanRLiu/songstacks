4ddf84a4efe6e5b8b7bbd840da4c6d18d4cd2a43
Nathan Liu <nathanliu17@gmail.com>
2022-12-07 17:39:38 -0800

    fix chinmays audio player (kind of)

--- frontend/src/Components/AudioPlayer.tsx
+++ frontend/src/Components/AudioPlayer.tsx
@@ -3,21 +3,19 @@ import { Buffer } from 'buffer';
 import ReactAudioPlayer from 'react-audio-player';
 import axios from 'axios';
 function AudioPlayer() {
-	const [audio, setAudio] = useState('');
+	const [audio, setAudio] = useState();
 	React.useEffect(() => {
 		const getUser = async () => {
-			const response = await axios.get(`/api/layer/playSong/?songid=63903c52655d9e5074bdc619`);
-			console.log(response.data);
-			const base64String = Buffer.from(response.data).toString('base64');
-			console.log(base64String);
-			setAudio(base64String);
-			// console.log(Buffer.from(response.data.chunk, 'base64'));
+			setAudio(new Audio(`/api/layer/playSong/?songid=63903c52655d9e5074bdc619`));
+
 		}
 		getUser();
 	}, []);
 	return (
 		<>
-			<ReactAudioPlayer controls src={`data:audio/mpeg;base64,${audio}`} />
+			{<div onClick={()=>{audio.play()}}style={{width:400,height:400}} />
+
+			}
 		</>
 	)
 	
