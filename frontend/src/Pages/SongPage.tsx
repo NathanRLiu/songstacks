e3052802ef0e8e2a4a17d9d95a8711d2cc9df8a3
Nathan Liu <nathanliu17@gmail.com>
2022-12-10 17:42:17 -0800

    song page

--- frontend/src/Pages/SongPage.tsx
+++ frontend/src/Pages/SongPage.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { BrowserRouter, Route, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import logo from '../songstacks.png';
@@ -7,71 +8,6 @@ import { AiOutlineSearch } from 'react-icons/ai'
 import StackView from '../Components/StackView';
 import NavBar from '../Components/NavBar';
 import styles from '../Styles/SongPage.module.css'
-
-const searchedLayers = [
-	{
-		"Title":"Layer 1",
-		"Type":"Beat",
-		"Length":"3:33",
-		"Cover":logo,
-		"Artist":"Nathan"
-	},
-	{
-		"Title":"Layer 1",
-		"Type":"Beat",
-		"Length":"3:33",
-		"Cover":logo,
-		"Artist":"Nathan"
-	},
-	{
-		"Title":"Layer 1",
-		"Type":"Beat",
-		"Length":"3:33",
-		"Cover":logo,
-		"Artist":"Nathan"
-	},
-	{
-		"Title":"Layer 1",
-		"Type":"Beat",
-		"Length":"3:33",
-		"Cover":logo,
-		"Artist":"Nathan"
-	},
-	{
-		"Title":"Layer 1",
-		"Type":"Beat",
-		"Length":"3:33",
-		"Cover":logo,
-		"Artist":"Nathan"
-	},
-	{
-		"Title":"Layer 1",
-		"Type":"Beat",
-		"Length":"3:33",
-		"Cover":logo,
-		"Artist":"Nathan"
-	},
-	{
-		"Title":"Layer 1",
-		"Type":"Beat",
-		"Length":"3:33",
-		"Cover":logo,
-		"Artist":"Nathan"
-	}, {
-		"Title":"Layer 1",
-		"Type":"Beat",
-		"Length":"3:33",
-		"Cover":logo,
-		"Artist":"Nathan"
-	},
-	{
-		"Title":"Layer 1",
-		"Type":"Beat",
-		"Length":"3:33",
-		"Cover":logo,
-		"Artist":"Nathan"
-	}
-]
 
 type SongCardProps = {
 	"Title":string;
@@ -95,10 +31,17 @@ function SongCard({ Title, Type, Length, Cover, Artist }: SongCardProps) {
 }
 function SongPage() {
 	let navigate = useNavigate();
+	const imageInput = useRef<HTMLInputElement>(null);
+
 	const [ title, setTitle ] = useState("");
+	const [published, publish] = useState(false);
 	const [ genre, setGenre ] = useState("");
+	const [ searchedLayers, setSearchedLayers ] = useState<{ID:string;Name:string;ParentLayer:string;ChildLayers:Array<any>;LayerCut:number}[]>([]);
+	const [parentLayer, setParentLayer] = useState("");
 
 	const [ description, setDescription ] = useState("");
+	const [ audioFile, setAudioFile ] = useState<null | Blob | string>();
+	const [ songImage, setSongImage ] = useState(logo);
 	return (
 	<div className={ styles.background } >
 		<NavBar />
@@ -113,44 +56,95 @@ function SongPage() {
 			<div className={ styles["detail-editor"] }>
 				<h1> Publish Layer </h1>
 				<div className={styles["detail-editor-flexc"]}>
-					<div className={styles["image-upload"]}>
-						<img src={logo}/>
+					<div className={styles["image-upload"]}
+						onClick={
+							()=>{
+								if (!imageInput.current) return
+								imageInput.current.click()
+							}
+						}
+					>
+						<input 
+							type="file"
+							accept="image/*" 
+							ref={imageInput}
+							onChange={
+								(event) => {
+									if (!event.target.files) return;
+									setSongImage(URL.createObjectURL(event.target.files[0]));
+								}
+							}
+						>
+						</input>
+						<img src={songImage}/>
 					</div>
 						
 					<div>
 						<div className={styles["search-bar"]}>
 							<input
-									type="text"
-									placeholder="Title"
-									value={title}
-									onChange={
-										(event) => {setTitle(event.target.value)}
-									}
+								type="text"
+								placeholder="Title"
+								value={title}
+								onChange={
+									(event) => {setTitle(event.target.value)}
+								}
 							/>
 						</div>
 						<div className={styles["search-bar"]} style={{marginTop:40}}>
 							<input
-									type="text"
-									placeholder="Genre"
-									value={genre}
-									onChange={
-										(event)=>{setGenre(event.target.value)}
-									}
+								type="text"
+								placeholder="Genre"
+								value={genre}
+								onChange={
+									(event)=>{setGenre(event.target.value)}
+								}
 							/>
 						</div>
 
 						<div className={styles["description"]}>
-							<textarea placeholder="Description"/>
+							<textarea
+								placeholder="Description"
+								value={description}
+								onChange={
+									(event) => {setDescription(event.target.value)}
+								}
+							/>
 						</div>
 					</div>
 				</div>
 				<div style={{"margin":30}}>
-						<input
-								type="file"
-						/>
+					<input
+						type="file"
+						onChange={
+							(event) => {
+								if (!event.target.files) return;
+								setAudioFile(URL.createObjectURL(event.target.files[0]));
+							}
+						}
+					/>
 				</div>
-				<button className={styles["publish-button"]}>
-					Publish
+				<button
+					className={styles["publish-button"]}
+					disabled = {published}
+					onClick={async ()=> {
+						const formData = new FormData();
+						
+						formData.append('description', description);
+						formData.append('name', title);
+						let cover = await fetch(songImage)
+							.then(r => r.blob())
+							.then(blobFile => new File([blobFile], "fileNameGoesHere", { type: "image/png" }))
+						formData.append("cover", cover);
+						publish(true);
+						axios.post('/api/layer/create',formData,
+						{
+							headers: {
+								'content-type': 'multipart/form-data'
+							}
+						});
+					}}
+				>
+					{published?"Published!":"Publish"}
 				</button>
 			</div>
 			<div className={ styles["editor-section"] } >
@@ -175,11 +169,11 @@ function SongPage() {
 						{searchedLayers.map((layerInfo, i) => (
 								<SongCard 
 									key={i}
-									Title={layerInfo.Title}
-									Type={layerInfo.Type}
-									Length={layerInfo.Length}
-									Artist={layerInfo.Artist}
-									Cover={layerInfo.Cover}
+									Title={layerInfo.Name}
+									Type="Beat"
+									Length="3:35"
+									Artist="Artist Name"
+									Cover=""
 								/>
 							))
 						}
